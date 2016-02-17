cf5f3c10fad229d7f0b19d98de02d01e8ed831be
Daniel Martinez <danmartinez101@gmail.com>
2016-02-17 01:07:48 -0500

    wip: JSXTransformer

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var webpack = require('webpack');
 var webpackStream = require('webpack-stream');
 var uglify = require('gulp-uglify');
 
-gulp.task('default', ['build']);
+gulp.task('default', ['build', 'build-jsx']);
 gulp.task('build', function() {
   return gulp.src('src/index.js')
     .pipe(webpackStream({
@@ -34,6 +34,54 @@ gulp.task('build', function() {
       output: {
         filename: 'babel.js',
         library: 'Babel',
+        libraryTarget: 'umd',
+      },
+      plugins: [
+        new webpack.DefinePlugin({
+          'process.env.NODE_ENV': '"production"'
+        }),
+        new webpack.optimize.OccurenceOrderPlugin(),
+        new webpack.optimize.DedupePlugin()
+      ]
+    }))
+    // Output unminified version
+    .pipe(gulp.dest('.'))
+
+    // Output minified version
+    .pipe(uglify())
+    .pipe(rename({ extname: '.min.js' }))
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('build-jsx', function() {
+  return gulp.src('src/index.js')
+    .pipe(webpackStream({
+      module: {
+        loaders: [
+          {
+            exclude: /node_modules/,
+            test: /\.js$/,
+            loader: 'babel',
+            query: {
+              presets: ['es2015', 'stage-0']
+            }
+          },
+          {
+            test: /\.json$/,
+            loader: 'json'
+          }
+        ]
+      },
+      node: {
+        // Mock Node.js modules that Babel require()s but that we don't
+        // particularly care about.
+        fs: 'empty',
+        module: 'empty',
+        net: 'empty'
+      },
+      output: {
+        filename: 'JSXTransformer.js',
+        library: 'JSXTransformer',
         libraryTarget: 'umd',
       },
       plugins: [
