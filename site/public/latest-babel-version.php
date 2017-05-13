498d702f72c663bd2486e1c9b846de3337c1a478
Daniel Lo Nigro <daniel@dan.cx>
2017-05-13 15:18:20 -0700

    Cache the Babel version in APC so it's not requested too often

--- site/public/latest-babel-version.php
+++ site/public/latest-babel-version.php
@@ -7,38 +7,50 @@ set_time_limit(120); // Just in case
 use GuzzleHttp\Client;
 use GuzzleHttp\Promise;
 
-// Get babel-standalone package.json
-$client = new Client();
-$response = $client->get('https://raw.githubusercontent.com/babel/babel-standalone/master/package.json', [
-  'headers' => [
-    'User-Agent' => 'Babel-Standalone-Update-Checker/1.0',
-  ],
-]);
-$manifest = json_decode((string)$response->getBody());
+const CACHE_KEY = 'babel_version';
 
-// Load all the package metadata for all the dependencies, in parallel
-$promises = [];
-foreach ($manifest->devDependencies as $name => $version) {
-  if (preg_match('/^babel\-(plugin|preset|core)/', $name)) {
-    $promises[$name] = $client->getAsync('https://registry.npmjs.com/'.$name, [
-      'headers' => [
-        'Accept' => 'application/vnd.npm.install-v1+json',
-        'User-Agent' => 'Babel-Standalone-Update-Checker/1.0',
-      ],
-    ]);
+function get_latest_version() {
+  // Get babel-standalone package.json
+  $client = new Client();
+  $response = $client->get('https://raw.githubusercontent.com/babel/babel-standalone/master/package.json', [
+    'headers' => [
+      'User-Agent' => 'Babel-Standalone-Update-Checker/1.0',
+    ],
+  ]);
+  $manifest = json_decode((string)$response->getBody());
+
+  // Load all the package metadata for all the dependencies, in parallel
+  $promises = [];
+  foreach ($manifest->devDependencies as $name => $version) {
+    if (preg_match('/^babel\-(plugin|preset|core)/', $name)) {
+      $promises[$name] = $client->getAsync('https://registry.npmjs.com/'.$name, [
+        'headers' => [
+          'Accept' => 'application/vnd.npm.install-v1+json',
+          'User-Agent' => 'Babel-Standalone-Update-Checker/1.0',
+        ],
+      ]);
+    }
   }
-}
-$responses = Promise\unwrap($promises);
+  $responses = Promise\unwrap($promises);
 
-// Find the latest version number
-$latest_version = '';
-foreach ($responses as $name => $response) {
-  $result = json_decode((string)$response->getBody());
-  $version = $result->{'dist-tags'}->latest;
-  if (version_compare($version, $latest_version, '>')) {
-    $latest_version = $version;
+  // Find the latest version number
+  $latest_version = '';
+  foreach ($responses as $name => $response) {
+    $result = json_decode((string)$response->getBody());
+    $version = $result->{'dist-tags'}->latest;
+    if (version_compare($version, $latest_version, '>')) {
+      $latest_version = $version;
+    }
   }
+  return $latest_version;
 }
 
-// Boom
+// Check if a value was already cached to throttle the number of times we send
+// network requests.
+$latest_version = apcu_fetch(CACHE_KEY);
+if (!$latest_version) {
+  $latest_version = get_latest_version();
+  apcu_store(CACHE_KEY, $latest_version, 60);
+}
+
 echo $latest_version;
